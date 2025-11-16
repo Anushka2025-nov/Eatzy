3a7154dbf354ce4f8871206df1c46e3b7ca2f896
Anushka2025-nov <anushkasrivastava823@gmail.com>
2025-11-16 17:00:29 +0530

    Initial commit

---
+++ Foody Hub Project/components/footer.js
@@ -0,0 +1,10 @@
+export const footer = () => {
+    return (
+        `<div id="footer">
+            <p class="text-center">Made By ~</p>
+            <p class="text-center" style="font-family: 'Source Sans Pro', sans-serif;">
+                Arun Kumar , Balvant Singh , Atharv Gupta , Anushka Shrivastav
+            </p>
+        </div>`
+    );
+};
